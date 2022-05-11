d19da02af067b9871039a4c6ef5aeb733f47273d
Cait-Si <cait@KeitonoMacBook-Air.local>
2022-05-11 13:39:50 +0900

    Add validate and i18n

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,18 @@ class Post < ApplicationRecord
   has_many :messages
 
   validates :title, :person, :datetime, :location, :level, :description, :deadline, presence: true
+  validate :date_before_start
+  validate :date_before_finish
 
   scope :by_date, -> { where(datetime: Date.today..).order(datetime: :desc) }
+
+  def date_before_start
+    return if datetime.blank?
+    errors.add(:datetime, "は今日以降のものを選択してください。") if datetime < Date.today
+  end
+
+  def date_before_finish
+    return if datetime.blank? || deadline.blank?
+    errors.add(:deadline, "は開始日前のものを選択してください") if datetime < deadline
+  end
 end
