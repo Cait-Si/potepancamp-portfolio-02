eb6983f244a7ed79ec2d330573c66aaf3d18615f
Cait-Si <cait@KeitonoMacBook-Air.local>
2022-05-19 11:51:39 +0900

    Remove message_broadcast_job.rb

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -8,6 +8,14 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    Message.create!(content: data['message'], user_id: current_user.id, post_id: data['post_id'])
+    new_message = Message.create!(content: data['message'], user_id: current_user.id, post_id: data['post_id'])
+    ActionCable.server.broadcast 'room_channel', {message: render_message(new_message)}
+  end
+
+  private
+
+
+  def render_message(message)
+    ApplicationController.renderer.render(partial: 'shared/message', locals: { message: message, current_user: current_user })
   end
 end
