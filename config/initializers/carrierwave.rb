d55550d574857b0d4ae18e9eac4164adadb3e5dd
Cait-Si <cait@KeitonoMacBook-Air.local>
2022-06-11 09:44:08 +0900

    Fix carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,10 +11,10 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
+      path_style: true
     }
 
     config.fog_directory  = 'potepan-portfolio-02'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/potepan-portfolio-02'
   else
     config.storage :file
   end
